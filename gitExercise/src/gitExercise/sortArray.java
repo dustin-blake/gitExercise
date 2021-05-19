33014db0ffcef4990da1f514386787a7fcb34870
dustin-blake <77656690+dustin-blake@users.noreply.github.com>
2021-05-19 01:13:08 -0700

    Created String Array
    Created an array of strings and then sorted it in alphabetical order

--- gitExercise/src/gitExercise/sortArray.java
+++ gitExercise/src/gitExercise/sortArray.java
@@ -1,9 +1,12 @@
 package gitExercise;
 
+import java.util.Arrays;
+
 public class sortArray {
 
 	public static void main(String[] args) {
 		int [] numArray = {6, 4, 89, 14, 63, 20, 27, 99, 1, 55, 70};
+		String [] fruits = {"grapes", "apples", "pears", "bananas", "oranges", "kiwi"};
 		int temp;
 		for(int i = 1; i < numArray.length; i++) {
 			for (int j = i; j > 0; j--) {
@@ -17,5 +20,7 @@ public class sortArray {
 		for(int i = 0; i < numArray.length; i++) {
 			System.out.println(numArray[i]);
 		}
+		Arrays.sort(fruits);
+		System.out.println(Arrays.toString(fruits));
 	}
 }
