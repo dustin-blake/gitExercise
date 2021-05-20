93ff1ee04fb3cd67dff457e74ce3b24980b8b2f2
dustin-blake <77656690+dustin-blake@users.noreply.github.com>
2021-05-19 21:08:19 -0700

    Updated Test of Int Array
    Compare new sorted integer array with the test to make sure it works.

--- gitExercise/src/gitExercise/sortArray.java
+++ gitExercise/src/gitExercise/sortArray.java
@@ -6,7 +6,10 @@ public class sortArray {
 
 	public static void main(String[] args) {
 		int [] numArray = {6, 4, 89, 14, 63, 20, 27, 99, 1, 55, 70};
+		int [] sortedNum = {1, 4, 6, 14, 20, 27, 55, 63, 70, 89, 99};
+		int [] newArray = new int[11];
 		String [] fruits = {"grapes", "apples", "pears", "bananas", "oranges", "kiwi"};
+		String [] sortedFruits = {"apples", "bananas", "grapes", "kiwi", "oranges", "pears"};
 		int temp;
 		for(int i = 1; i < numArray.length; i++) {
 			for (int j = i; j > 0; j--) {
@@ -18,8 +21,18 @@ public class sortArray {
 			}
 		}
 		for(int i = 0; i < numArray.length; i++) {
-			System.out.println(numArray[i]);
+			newArray[i] = numArray[i];
 		}
+		
+		//@Test
+		if(Arrays.equals(newArray, sortedNum)) {
+			System.out.println(Arrays.toString(newArray));
+			System.out.println("The array is sorted in ascending order");
+		}
+		else {
+			System.out.println("The array is not sorted");
+		}
+		
 		Arrays.sort(fruits);
 		
 		//@Test
